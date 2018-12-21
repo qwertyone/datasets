7d88cae8fb7bc541b0e8e5ca99886aa1ff3b0bd8
Aaron G. (anotherhuman) <qwertyone@users.noreply.github.com>
2018-12-21 12:13:26 +0100

    Create DetectAlgae.py

---
+++ DetectAlgae.py
@@ -0,0 +1,74 @@
+#####################################
+# Libraries
+#####################################
+# Common libs
+import pandas as pd
+import numpy as np
+import sys
+import os
+import random
+from pathlib import Path
+
+# Image processing
+import imageio
+import skimage
+import skimage.io
+import skimage.transform
+#from skimage.transform import rescale, resize, downscale_local_mean
+
+# Charts
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ML
+import scipy
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+#from sklearn.preprocessing import OneHotEncoder
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Activation, Flatten, MaxPool2D, Dropout, BatchNormalization,LeakyReLU
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping, ReduceLROnPlateau
+from keras.utils import to_categorical
+import tensorflow
+
+#####################################
+# Settings
+#####################################
+
+# Set random seed to make results reproducable
+np.random.seed(42)
+tensorflow.set_random_seed(42)
+
+# Global variables
+img_folder='.../IMGS/'
+img_width=100
+img_height=100
+img_channels=3
+
+algae=pd.read_csv('.../IMGS/images_labels.csv', 
+                index_col=False,  
+                dtype={'algae_from_space':'category','other':'category'})
+
+# Will use this function later to load images of preprocessed bees
+# Don't load images just from the start to save memory for preprocessing steps
+def read_img(file):
+    """
+    Read and resize img, adjust channels. 
+    Caution: This function is not independent, it uses global vars: img_folder, img_channels
+    @param file: file name without full path
+    """
+    img = skimage.io.imread(img_folder + file)
+    img = skimage.transform.resize(img, (img_width, img_height), mode='reflect')
+    return img[:,:,:img_channels]
+
+# Cannot impute nans, drop them
+algae.dropna(inplace=True)
+
+# Some image files don't exist. Leave only bees with available images.
+#img_exists = algae['file'].apply(lambda f: os.path.exists(img_folder + f))
+#algae = algae[img_exists]
+
+algae
